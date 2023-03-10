1e0e49a2637b3640407ae8f18d7e1aced4dba93e
tejavamshi <tejavamshikappala@gmail.com>
2023-03-10 17:55:49 +0000

    forFaqs

---
+++ src/components/FaqItem/index.js
@@ -0,0 +1,63 @@
+import {Component} from 'react'
+
+import './index.css'
+
+const PLUS = 'https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png'
+const MINUS = 'https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png'
+
+class FaqItem extends Component {
+  state = {
+    isActive: false,
+  }
+
+  renderAnswer = () => {
+    const {faqDetails} = this.props
+    const {answerText} = faqDetails
+    const {isActive} = this.state
+
+    if (isActive) {
+      return (
+        <div>
+          <hr className="horizontal-line" />
+          <p className="answer">{answerText}</p>
+        </div>
+      )
+    }
+    return null
+  }
+
+  onToggleIsActive = () => {
+    this.setState(prevState => ({
+      isActive: !prevState.isActive,
+    }))
+  }
+
+  renderActiveImage = () => {
+    const {isActive} = this.state
+    const imageLink = isActive ? MINUS : PLUS
+    const altText = isActive ? 'minus' : 'plus'
+
+    return (
+      <button className="button" type="button" onClick={this.onToggleIsActive}>
+        <img className="image" src={imageLink} alt={altText} />
+      </button>
+    )
+  }
+
+  render() {
+    const {faqDetails} = this.props
+    const {questionText} = faqDetails
+
+    return (
+      <li className="faq-item">
+        <div className="question-container">
+          <h1 className="question">{questionText}</h1>
+          {this.renderActiveImage()}
+        </div>
+        {this.renderAnswer()}
+      </li>
+    )
+  }
+}
+
+export default FaqItem
